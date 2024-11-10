fe09c3b2dd549db28bc504cf914dbda50132ef6c
Ritabrata123github <121775173+Ritabrata123github@users.noreply.github.com>
2024-11-10 18:38:44 +0530

    Create connection.php

---
+++ database/connection.php
@@ -0,0 +1,10 @@
+<?php
+// database connection for Minglr
+$host = "localhost";
+$user = "root";
+$pswd = "";
+$db = "Social_Media_App";
+
+// create connect to db
+$connection = mysqli_connect($host, $user, $pswd, $db);
+?>
